0a992d1501b9f6dd0dabfa5bd7d5f23acc9e7535
Devin Austin <c-devin.austin@charter.com>
2019-01-03 14:59:01 -0700

    switched to akka http

--- src/main/scala/io/dja/smough/ApiServer.scala
+++ src/main/scala/io/dja/smough/ApiServer.scala
@@ -1,24 +1,41 @@
 package io.dja.smough
-import cats.effect._
-import org.http4s._
-import org.http4s.dsl.io._
-import fs2.{Stream, StreamApp}
-import fs2.StreamApp.ExitCode
-import org.http4s.server.blaze._
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.{ContentType, HttpEntity, HttpResponse, MediaTypes}
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 
-object ApiServer extends StreamApp[IO] {
+object ApiServer extends WithLogger {
+  implicit private val system: ActorSystem = ActorSystem("smough-rest-api")
+  implicit private val executor: ExecutionContext = system.dispatcher
+  implicit private val materializer: ActorMaterializer = ActorMaterializer()
 
-  val postsService = HttpService[IO] {
-    case GET -> Root / "posts" =>
-      Ok(s"Ok")
+  val postsEndpoint = path("posts") {
+    get
+    complete(
+      HttpResponse(
+        entity = HttpEntity(
+          ContentType(MediaTypes.`application/json`), "[]")))
   }
 
-  override def stream(args: List[String], requestShutdown: IO[Unit]): Stream[IO, ExitCode] = {
-    BlazeBuilder[IO]
-      .bindHttp(8080, "localhost")
-      .mountService(postsService, "/api")
-      .serve
+  private val bindingFuture =
+    Http().bindAndHandle(postsEndpoint, "0.0.0.0", 8080)
+
+  def main(args: Array[String]): Unit = {
+    bindingFuture.onComplete {
+      case Success(serverBinding) =>
+        log.info(s"listening to ${serverBinding.localAddress}")
+      case Failure(error) => println(s"error: ${error.getMessage}")
+    }
   }
+
+
 }
 
+import org.slf4j.{LoggerFactory, Logger => SLogger}
+
+trait WithLogger {
+  lazy val log: SLogger = LoggerFactory.getLogger(getClass)
+}
