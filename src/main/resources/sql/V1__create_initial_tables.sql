10b8a13bef9707cd31135efebe14410b6397c6bc
Devin Austin <c-devin.austin@charter.com>
2019-03-11 16:22:21 -0600

    bumped up pg version for travis, added index for slug, added index for posts on a certain date, added published_on column

--- src/main/resources/sql/V1__create_initial_tables.sql
+++ src/main/resources/sql/V1__create_initial_tables.sql
@@ -4,8 +4,13 @@ CREATE TABLE post(
   title varchar(200) not null,
   body text not null, -- TODO: add tsearch indexes and stuff for this
   author int not null,
-  slug varchar(100) not null, -- TODO: add a unique constraint here
+  slug varchar(100) not null,
+  published_on timestamptz not null default now(), -- default to publishing immediately
   -- TODO: add indexes for these
   created_on timestamptz not null default now(),
   updated_on timestamptz not null default now()
 );
+
+CREATE UNIQUE INDEX post_slug_idx ON post(slug);
+
+CREATE INDEX posts_on_day ON post(date(published_on));
