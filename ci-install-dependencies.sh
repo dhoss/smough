9eb597855be57cf16b85e00c2e4421ee53df5848
Devin Austin <c-devin.austin@charter.com>
2019-03-12 14:02:59 -0600

    cleanup

--- ci-install-dependencies.sh
+++ ci-install-dependencies.sh
@@ -3,5 +3,4 @@
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter &&
 chmod +x ./cc-test-reporter &&
 wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/6.0.0-beta/flyway-commandline-6.0.0-beta-linux-x64.tar.gz | tar xvz && sudo ln -s `pwd`/flyway-6.0.0-beta/flyway /usr/local/bin || true &&
-psql -U postgres < src/main/resources/sql/test-bootstrap.sql #&&
-#./migrate-db.sh smough_test smough_test smough_test migrate
+psql -U postgres < src/main/resources/sql/test-bootstrap.sql
